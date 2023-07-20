5ae3fb879bd63aa18879ae2cc32b5e9f3934acad
jonas-1997 <jvhencke@googlemail.com>
2023-07-20 13:05:25 +0200

    install react-router-dom / create Layout components

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <Link to="/">Testlink</Link>
+  );
+}
+
+ /* <header>
+      <Link className="page-logo" to="/">
+        BookNook
+      </Link>
+      <nav>
+        <NavLink
+          to="/books"
+        >
+          Bücher
+        </NavLink>
+        <NavLink
+          to="/about"
+        >
+          Über uns
+        </NavLink>
+        <NavLink
+          to="/contact"
+        >
+          Kontakt
+        </NavLink>
+      </nav>
+    </header> */
