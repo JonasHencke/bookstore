b39ca9b5cd1ab19ddd4f1b40902ebadabd982a59
jonas-1997 <jvhencke@googlemail.com>
2023-07-21 19:48:10 +0200

    add banner and text to Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import banner from "../images/banner.jpeg";
 
 export default function Home() {
-    return(
-        <p>Homepage</p>
-    )
+  return (
+    <>
+      <div className="banner-container">
+        <img className="banner-img" src={banner}></img>
+        <button className="banner-button">Shop</button>
+      </div>
+      <div className="text-block">
+        <h2>Lorem ipsum dolor</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum
+        </p>
+      </div>
+    </>
+  );
 }
