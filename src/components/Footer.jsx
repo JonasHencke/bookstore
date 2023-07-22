4a3ff4d34cac96d30b388e3fc2c21cdae3d80bb1
jonas-1997 <jvhencke@googlemail.com>
2023-07-22 22:34:50 +0200

    add content to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,45 @@
-import React from "react"
+import React from "react";
 
 export default function Footer() {
-    return (
-    <footer>&#169; 2023 BookNooK</footer>
-    )
+  return (
+    <>
+      <div className="newsletter-wrapper">
+        <h3>LOREM IPSUM DOLOR SIT AMET CONSETETUR</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+        </p>
+        <form>
+            <input></input>
+            <button className="newsletter-button">SUBSCRIBE</button>
+        </form>
+      </div>
+      <div className="imprint">
+      <div className="imprint-flex">
+        <div className="imprint-container">
+            <h3>Lorem ipsum</h3>
+            <p>vero eos et accusam</p>
+            <p>vero eos et accusam</p>
+            <p>vero eos et accusam</p>
+            <p>vero eos et accusam</p>
+        </div>
+        <div className="imprint-container">
+            <h3>Lorem ipsum</h3>
+            <p>vero eos et accusam</p>
+            <p>vero eos et accusam</p>
+            <p>vero eos et accusam</p>
+            <p>vero eos et accusam</p>
+        </div>
+        <div className="imprint-container">
+            <h3>Lorem ipsum</h3>
+            <p>vero eos et accusam</p>
+            <p>vero eos et accusam</p>
+            <p>vero eos et accusam</p>
+            <p>vero eos et accusam</p>
+        </div>
+        </div>
+      </div>
+      <footer>&#169; 2023 BookNooK</footer>
+    </>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,6 +17,14 @@ export default function Home() {
           rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
           ipsum
         </p>
+        <h2>Lorem ipsum dolor</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum
+        </p>
       </div>
     </>
   );
