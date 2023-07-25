60eb3bc214c538b19ea88b0990f8f6946da04367
jonas-1997 <jvhencke@googlemail.com>
2023-07-25 20:02:14 +0200

    add Links to filter displayed books by genre

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,32 +1,49 @@
 import React from "react";
-import { getBooks } from '../server.jsx'
+import { getBooks } from "../server.jsx";
+import { Link, useSearchParams } from "react-router-dom";
 
 export default function Books() {
-    const [books, setBooks] = React.useState([])
-    
-    React.useEffect(() => {
-        async function loadBooks() {
-            let data = await getBooks();
-            setBooks(data)
-        }
-        loadBooks()
-    }, [])
+  const [books, setBooks] = React.useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
 
-    const displayedBooks = books.map(book => (
-        <div key={book.id} className="book-card">
-            <img src={book.imageURL} style={{width: "200px"}}></img>
-            <div className="book-info">
-                <p style={{fontWeight: "300"}}>{book.author}</p>
-                <p style={{fontWeight: "500"}}>{book.title}</p>
-                <p style={{fontWeight: "500"}}>{book.price} €</p>
-                <button className="warenkorb-Btn">In den Warenkorb</button>
-            </div>
-        </div>
-    ))
-    console.log(books)
-    return(
-        <div className="shop-display">
-            <div className="book-display">{displayedBooks}</div>
-        </div>
-    )
+  React.useEffect(() => {
+    async function loadBooks() {
+      let data = await getBooks();
+      setBooks(data);
+    }
+    loadBooks();
+  }, []);
+
+  const typeFilter = searchParams.get("type");
+  const filteredBooks = typeFilter
+    ? books.filter((book) => book.genre.toLowerCase() === typeFilter)
+    : books;
+
+  const displayedBooks = filteredBooks.map((book) => (
+    <div key={book.id} className="book-card">
+      <img src={book.imageURL} style={{ width: "200px" }}></img>
+      <div className="book-info">
+        <p style={{ fontWeight: "300" }}>{book.author}</p>
+        <p style={{ fontWeight: "500" }}>{book.title}</p>
+        <p style={{ fontWeight: "500" }}>{book.price} €</p>
+        <button className="warenkorb-Btn">In den Warenkorb</button>
+      </div>
+    </div>
+  ));
+  console.log(books);
+  return (
+    <>
+      <div className="search-buttons">
+        <Link to="?type=fantasy">Fantasy</Link>
+        <Link to="?type=klassiker">Klassiker</Link>
+        <Link to="?type=sachbuch">Sachbuch</Link>
+        <Link to="?type=kinder und jugend">Kinder und Jugend</Link>
+        <Link to="?type=roman">Roman</Link>
+        <Link to="?type=krimi">Krimi</Link>
+      </div>
+      <div className="shop-display">
+        <div className="book-display">{displayedBooks}</div>
+      </div>
+    </>
+  );
 }
