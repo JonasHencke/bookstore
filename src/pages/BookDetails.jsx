273d127da9b9cfff57a02aba70c08c4289828e54
jonas-1997 <jvhencke@googlemail.com>
2023-07-31 15:37:38 +0200

    add styling to BooksDetails component

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -19,18 +19,18 @@ export default function BookDetails() {
       console.log(location.state.search)
     return (
       <>
-        <Link to={`/books/${location.state.search}`}>← Zurück</Link>
+        <Link className="bookdetails-backlink" to={`/books/${location.state.search}`}>← Zurück</Link>
         <div className="bookdetails-container">
         <img src={book.imageURL} className="bookdetails-image"/>
         <div className="bookdetails-information">
           <h1 className="bookdetails-title">{book.title}</h1>
           <h2 className="bookdetails-author">{book.author}</h2>
           <h2 className="bookdetails-price">Preis: {book.price} €</h2>
+          <p className="bookdetails-genre">Genre: {book.genre}</p>
           <p className="bookdetails-pages">Seitenanzahl: {book.pages}</p>
           <p className="bookdetails-releaseDate">Erscheinungsdatum: {book.releaseDate}</p>
           <p className="bookdetails-ISBN">ISBN: {book.ISBN}</p>
           <p className="bookdetails-description">Beschreibung: <br/><br/> {book.description}</p>
-          <p className="bookdetails-genre">Genre: {book.genre}</p>
         </div>
         </div>
       </>
