b1a4325e79e188104ead818ed9168e3f894d3366
jonas-1997 <jvhencke@googlemail.com>
2023-08-02 22:04:17 +0200

    add context and item counter to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import ShoppingCartSVG from "../images/shoppingCart.svg";
+import { ThemeContext } from "../App.jsx";
 
 export default function Header() {
+  const { shoppingCart } = React.useContext(ThemeContext);
+
   return (
     <header>
-        <Link className="page-logo" to="/">
-          BookNook
-          <p>LIES DICH GLÜCKLICH</p>
-          <img className="shoppingCart-icon" src={ShoppingCartSVG}/>
-        </Link>
+      <Link className="page-logo" to="/">
+        BookNook
+        <p>LIES DICH GLÜCKLICH</p>
+        <div className="shoppingCart-wrapper">
+          <img className="shoppingCart-icon" src={ShoppingCartSVG} />
+          <p className="cart-counter">{shoppingCart.length}</p>
+        </div>
+      </Link>
       <nav className="header-menu">
         <NavLink to="/books?page=1">Bücher</NavLink>
         <NavLink to="/about">Über uns</NavLink>
