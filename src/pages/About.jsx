62f2990ca2541f38f811a62dae6d73c78d2c74ec
jonas-1997 <jvhencke@googlemail.com>
2023-08-06 09:42:41 +0200

    Add HTML structure to About component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,20 +2,49 @@ import React from "react";
 
 export default function About() {
   return (
-    <>
-      <h1>Lorem ipsum dolor</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. no sea takimata sanctus est Lorem ipsum dolor sit
-        amet.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. no sea takimata sanctus est Lorem ipsum dolor sit
-        amet.
-      </p>
-    </>
+    <div className="about">
+      <div className="about-banner">
+        <h1>Über BookNook</h1>
+        <p className="about-quote">
+          Von allen Welten, die der Mensch erschaffen hat, ist die der Bücher
+          die Gewaltigste.
+        </p>
+        <p>- Heinrich Heine -</p>
+      </div>
+      <div className="about-display">
+        <h2></h2>
+        <p></p>
+        <div>
+          <div>
+            <img></img>
+            <p></p>
+            <p></p>
+          </div>
+          <div>
+            <img></img>
+            <p></p>
+            <p></p>
+          </div>
+          <div>
+            <img></img>
+            <p></p>
+            <p></p>
+          </div>
+        </div>
+      </div>
+      <div className="staff-banner">
+        <img></img>
+        <div>
+          <h2></h2>
+          <p></p>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
