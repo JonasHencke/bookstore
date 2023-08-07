e8c5c716af49b87e581efaa73e043613e833b1c3
jonas-1997 <jvhencke@googlemail.com>
2023-08-07 17:14:17 +0200

    Add Basic HTML structure for Contact component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
 export default function Contact() {
-    return(
-        <p>Contact</p>
-    )
+  return (
+    <>
+      <p>wir würden gerne mehr von dir hören. Kontaktiere uns per E-Mail</p>
+      <div>
+        <div>
+          <h1>Kontaktformular</h1>
+          <form>
+            <label></label>
+            <input></input>
+            <label></label>
+            <input></input>
+            <label></label>
+            <input></input>
+            <button></button>
+          </form>
+        </div>
+        <div>
+            <div>
+                <img></img>
+                <h3></h3>
+                <p></p>
+            </div>
+            <div>
+                <img></img>
+                <h3></h3>
+                <p></p>
+            </div>
+            <div>
+                <img></img>
+                <h3></h3>
+                <p></p>
+            </div>
+        </div>
+      </div>
+    </>
+  );
 }
