2d90224e82e5166fe946482d17ada055f4e933db
jonas-1997 <jvhencke@googlemail.com>
2023-08-07 13:04:23 +0200

    Finish About component layout and styling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import EarthSVG from "../images/earth.svg"
-import CustomerSVG from "../images/customer.svg"
-import BooksSVG from "../images/books.svg"
+import EarthSVG from "../images/earth.svg";
+import CustomerSVG from "../images/customer.svg";
+import BooksSVG from "../images/books.svg";
 
 export default function About() {
   return (
@@ -16,41 +16,56 @@ export default function About() {
       </div>
       <div className="about-display">
         <h2>Unsere Versprechen</h2>
-        <div>
-          <div>
+        <div className="promises-display">
+          <div className="promise-card">
             <img src={CustomerSVG} className="about-SVG"></img>
-            <p>Rücksicht auf die Kunden</p>
-            <p>zuvorkommend, verständisvoll, freundlich</p>
+            <p className="promise-card-head">Rücksicht auf die Kunden</p>
+            <p className="promise-card-text">
+              zuvorkommend, verständisvoll, freundlich
+            </p>
           </div>
-          <div>
+          <div className="promise-card">
             <img src={EarthSVG} className="about-SVG"></img>
-            <p>Rücksicht auf die Umwelt</p>
-            <p>
+            <p className="promise-card-head">Rücksicht auf die Umwelt</p>
+            <p className="promise-card-text">
               nachhaltiger Versand,
               <br /> Abholung in der Filliale möglich
             </p>
           </div>
-          <div>
+          <div className="promise-card">
             <img src={BooksSVG} className="about-SVG"></img>
-            <p>Rücksicht auf die Literatur</p>
-            <p>
+            <p className="promise-card-head">Rücksicht auf die Literatur</p>
+            <p className="promise-card-text">
               vielfältiges Sortiment,
               <br />
             </p>
           </div>
         </div>
       </div>
-      <div className="staff-banner">
-        <img></img>
-        <div>
-          <h2></h2>
-          <p></p>
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
+      <div className="about-description">
+        <div className="about-description-left">
+          <h2>lorem ipsum dolor</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata. labore et
+            dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo
+          </p>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et
+          </p>
+        </div>
+        <div className="about-description-right">
+          <p className="about-description-bold">lorem ipsum dolor</p>
+          <p>At vero eos et accusam et justo</p>
+          <br/>
+          <p className="about-description-bold">lorem ipsum dolor</p>
+          <p>At vero eos et accusam et justo</p>
         </div>
       </div>
     </div>
