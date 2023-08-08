8f25a2ab357d35db56f8f9a1c328dac8584084a8
jonas-1997 <jvhencke@googlemail.com>
2023-08-08 17:40:40 +0200

    Update HTML elements with content and classnames in Contact component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,38 +1,51 @@
 import React from "react";
+import AddressSVG from "../images/address.svg"
+import MailSVG from "../images/mail.svg"
+import TelephoneSVG from "../images/telephone.svg"
 
 export default function Contact() {
   return (
     <>
-      <p>wir würden gerne mehr von dir hören. Kontaktiere uns per E-Mail</p>
+      <p className="contact-message">wir würden gerne mehr von dir hören. Kontaktiere uns per E-Mail</p>
       <div>
         <div>
-          <h1>Kontaktformular</h1>
-          <form>
-            <label></label>
-            <input></input>
-            <label></label>
-            <input></input>
-            <label></label>
-            <input></input>
-            <button></button>
+          <h1 className="contact-header">Kontaktformular</h1>
+          <form className="contact-forms">
+            <div className="contact-forms-container">
+              <label>Name</label>
+              <input></input>
+            </div>
+            <div className="contact-forms-container">
+              <label>E-Mail</label>
+              <input></input>
+            </div>
+            <div className="contact-forms-container">
+              <label>Telefonnummer</label>
+              <input></input>
+            </div>
+            <div className="contact-forms-container">
+              <label>Nachricht</label>
+              <input></input>
+            </div>
+            <button className="contact-forms-button">Senden</button>
           </form>
         </div>
         <div>
-            <div>
-                <img></img>
-                <h3></h3>
-                <p></p>
-            </div>
-            <div>
-                <img></img>
-                <h3></h3>
-                <p></p>
-            </div>
-            <div>
-                <img></img>
-                <h3></h3>
-                <p></p>
-            </div>
+          <div>
+            <img src={AddressSVG} style={{width: "40px"}}></img>
+            <h3>Adresse</h3>
+            <p>12345 Musterstadt <br/> BookNook GmbH <br/> 12345</p>
+          </div>
+          <div>
+            <img src={TelephoneSVG} style={{width: "40px"}}></img>
+            <h3>Ruf uns an</h3>
+            <p>+49 123 45678</p>
+          </div>
+          <div>
+            <img src={MailSVG} style={{width: "40px"}}></img>
+            <h3>Schreib uns</h3>
+            <p>book@nook.de</p>
+          </div>
         </div>
       </div>
     </>
