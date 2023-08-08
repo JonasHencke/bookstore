d156052739c6b6f1938207638b482aedcada0996
jonas-1997 <jvhencke@googlemail.com>
2023-08-08 18:45:42 +0200

    Add Styling to Contact component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import AddressSVG from "../images/address.svg"
-import MailSVG from "../images/mail.svg"
-import TelephoneSVG from "../images/telephone.svg"
+import AddressSVG from "../images/address.svg";
+import MailSVG from "../images/mail.svg";
+import TelephoneSVG from "../images/telephone.svg";
 
 export default function Contact() {
   return (
-    <>
-      <p className="contact-message">wir würden gerne mehr von dir hören. Kontaktiere uns per E-Mail</p>
-      <div>
-        <div>
+    <div className="contact-wrapper">
+      <p className="contact-message">
+        wir würden gerne mehr von dir hören. Kontaktiere uns per E-Mail
+      </p>
+      <div className="contact-display">
+        <div className="contact-display-forms">
           <h1 className="contact-header">Kontaktformular</h1>
           <form className="contact-forms">
             <div className="contact-forms-container">
@@ -30,24 +32,26 @@ export default function Contact() {
             <button className="contact-forms-button">Senden</button>
           </form>
         </div>
-        <div>
+        <div className="contact-display-info">
           <div>
-            <img src={AddressSVG} style={{width: "40px"}}></img>
+            <img src={AddressSVG} style={{ width: "40px" }}></img>
             <h3>Adresse</h3>
-            <p>12345 Musterstadt <br/> BookNook GmbH <br/> 12345</p>
+            <p>
+              12345 Musterstadt <br /> BookNook GmbH <br /> 12345
+            </p>
           </div>
           <div>
-            <img src={TelephoneSVG} style={{width: "40px"}}></img>
+            <img src={TelephoneSVG} style={{ width: "40px" }}></img>
             <h3>Ruf uns an</h3>
             <p>+49 123 45678</p>
           </div>
           <div>
-            <img src={MailSVG} style={{width: "40px"}}></img>
+            <img src={MailSVG} style={{ width: "40px" }}></img>
             <h3>Schreib uns</h3>
             <p>book@nook.de</p>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
