08e08dab239f8ad039b7bd4c21eecab2e8299a2d
jonas-1997 <jvhencke@googlemail.com>
2023-08-11 13:26:54 +0200

    Add conditional rendering to BookDetails page

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -13,48 +13,86 @@ export default function BookDetails() {
     async function loadBooks() {
       let data = await getBooks();
       let filteredBook = data.filter((book) => book.id === id);
-      console.log(filteredBook)
+      console.log(filteredBook);
       setBook(filteredBook[0]);
     }
     loadBooks();
   }, [id]);
-
-  return (
-    <>
-      <Link
+  if (location.state === null) {
+    return (
+      <>
+        <div className="bookdetails-container">
+          <img src={book.imageURL} className="bookdetails-image" />
+          <div className="bookdetails-information">
+            <h1 className="bookdetails-title">{book.title}</h1>
+            <h2 className="bookdetails-author">{book.author}</h2>
+            <h2 className="bookdetails-price">Preis: {book.price} €</h2>
+            <button
+              className="warenkorb-Btn"
+              style={{ alignSelf: "start", marginBottom: "1em" }}
+              onClick={(e) => {
+                addItemToCart(book);
+                e.preventDefault();
+              }}
+            >
+              In den Warenkorb
+            </button>
+            <p className="bookdetails-genre">Genre: {book.genre}</p>
+            <p className="bookdetails-pages">Seitenanzahl: {book.pages}</p>
+            <p className="bookdetails-releaseDate">
+              Erscheinungsdatum: {book.releaseDate}
+            </p>
+            <p className="bookdetails-ISBN">ISBN: {book.ISBN}</p>
+            <p className="bookdetails-description">
+              Beschreibung: <br />
+              <br /> {book.description}
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Link
         className="bookdetails-backlink"
         to={location.state.search}
       >
         ← Zurück
       </Link>
-      <div className="bookdetails-container">
-        <img src={book.imageURL} className="bookdetails-image" />
-        <div className="bookdetails-information">
-          <h1 className="bookdetails-title">{book.title}</h1>
-          <h2 className="bookdetails-author">{book.author}</h2>
-          <h2 className="bookdetails-price">Preis: {book.price} €</h2>
-          <button
-            className="warenkorb-Btn"
-            style={{ alignSelf: "start", marginBottom: "1em" }}
-            onClick={(e) => {
-              addItemToCart(book);
-              e.preventDefault();
-            }}
-          >
-            In den Warenkorb
-          </button>
-          <p className="bookdetails-genre">Genre: {book.genre}</p>
-          <p className="bookdetails-pages">Seitenanzahl: {book.pages}</p>
-          <p className="bookdetails-releaseDate">
-            Erscheinungsdatum: {book.releaseDate}
-          </p>
-          <p className="bookdetails-ISBN">ISBN: {book.ISBN}</p>
-          <p className="bookdetails-description">
-            Beschreibung: <br />
-            <br /> {book.description}
-          </p>
+        <div className="bookdetails-container">
+          <img src={book.imageURL} className="bookdetails-image" />
+          <div className="bookdetails-information">
+            <h1 className="bookdetails-title">{book.title}</h1>
+            <h2 className="bookdetails-author">{book.author}</h2>
+            <h2 className="bookdetails-price">Preis: {book.price} €</h2>
+            <button
+              className="warenkorb-Btn"
+              style={{ alignSelf: "start", marginBottom: "1em" }}
+              onClick={(e) => {
+                addItemToCart(book);
+                e.preventDefault();
+              }}
+            >
+              In den Warenkorb
+            </button>
+            <p className="bookdetails-genre">Genre: {book.genre}</p>
+            <p className="bookdetails-pages">Seitenanzahl: {book.pages}</p>
+            <p className="bookdetails-releaseDate">
+              Erscheinungsdatum: {book.releaseDate}
+            </p>
+            <p className="bookdetails-ISBN">ISBN: {book.ISBN}</p>
+            <p className="bookdetails-description">
+              Beschreibung: <br />
+              <br /> {book.description}
+            </p>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    )
+  }
+}
+
+{
+  /*  */
 }
