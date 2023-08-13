99885e82f3c34351cbe68306215b0be9da261260
jonas-1997 <jvhencke@googlemail.com>
2023-08-13 09:48:13 +0200

    added snackbar Component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "./Header"
 import Footer from "./Footer"
 import { Outlet } from "react-router-dom"
+import Snackbar from "./Snackbar";
 
 export default function Layout() {
     return (
         <div className="page-layout">
             <Header/>
+            <Snackbar/>
             <main>
               <Outlet />
             </main>
---
+++ src/components/Snackbar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Snackbar() {
+    React.useRef()
+    
+    
+    return (
+        <div id="snackbar-visible" className="snackbar">
+            <p>Deine Nachricht wurde versandt!</p>
+        </div>
+    )
+}
